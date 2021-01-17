734cfec63b71870c1c2621f06196394094ad2ae0
MariaMoreno <maru_200202@yahoo.com.ar>
2021-01-17 19:06:08 -0300

    Updates

---
+++ precache-manifest.9194b7002975c38da1ad3ead309cc4ba.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24c34a00b515b832ca4b5aafbfe17e7",
+    "url": "/Rick-and-Morty-app/index.html"
+  },
+  {
+    "revision": "711c45613d205dbd1673",
+    "url": "/Rick-and-Morty-app/static/css/2.32a1eed3.chunk.css"
+  },
+  {
+    "revision": "11bd5e4d0b7e01d16e2f",
+    "url": "/Rick-and-Morty-app/static/css/main.4c4b34fa.chunk.css"
+  },
+  {
+    "revision": "711c45613d205dbd1673",
+    "url": "/Rick-and-Morty-app/static/js/2.b03856a8.chunk.js"
+  },
+  {
+    "revision": "535633f1b2b7b07695fad204a5c5869b",
+    "url": "/Rick-and-Morty-app/static/js/2.b03856a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11bd5e4d0b7e01d16e2f",
+    "url": "/Rick-and-Morty-app/static/js/main.769ef8fa.chunk.js"
+  },
+  {
+    "revision": "7789d57b104ef8535b5b",
+    "url": "/Rick-and-Morty-app/static/js/runtime-main.d8cb14e2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Rick-and-Morty-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Rick-and-Morty-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Rick-and-Morty-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Rick-and-Morty-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Rick-and-Morty-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
